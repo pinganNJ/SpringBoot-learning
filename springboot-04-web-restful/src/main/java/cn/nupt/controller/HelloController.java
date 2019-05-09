5bc4ce41eb59b4d5ee2900df0b658fbecb463817
pingan <pizan@foxmail.com>
2019-05-09 11:59:51 +0800

    controller返回map，然后前端用thymeleaf捕获

--- springboot-04-web-restful/src/main/java/cn/nupt/controller/HelloController.java
+++ springboot-04-web-restful/src/main/java/cn/nupt/controller/HelloController.java
@@ -1,7 +1,11 @@
 package cn.nupt.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @AUTHOR PizAn
@@ -9,15 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 
-@RestController
+@Controller
 public class HelloController {
-
+    @ResponseBody
     @RequestMapping("/hello")
-    public String  hello(){
-        return  "aaa";
+    public String hello() {
+        return "aaa";
     }
 
 
+    @RequestMapping("/success")
+    public String success(Map<String, Object> map) {
+        //返回map，然后前端用thymeleaf捕获
+        map.put("hello","springboot");
+        return "success";
+
+    }
 
 
 }
